cdcbcccfb8671ac48ed90559bf90dbc26ad9bfb8
azat baran <azat_2600@hotmail.com>
2022-11-23 01:55:02 +0100

    big changes

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -1,26 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+var Builder = WebApplication.CreateBuilder(args);
 
-namespace API
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+
+Builder.Services.AddControllers();
+
+var app = Builder.Build();
+
+// Configuer the HTTP request pipeline.
+app.MapControllers();
+
+app.Run();
